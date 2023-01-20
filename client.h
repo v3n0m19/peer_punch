f4d712419318aaca866a9815f4e3833a3200971c
v3nom <v3nom@dsc.com>
2023-01-20 08:51:57 +0530

    TEST #3

--- client.h
+++ client.h
@@ -7,45 +7,71 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <pthread.h>
+#include <ncurses.h>
 
 #define MAXLINE 1024
+#define MAXSIZE 1024
+
 typedef struct
 {
     int sockfd;
     struct sockaddr_in servaddr;
+    WINDOW *ibox;
+    WINDOW *sbox;
+    WINDOW *rbox;
 } func_args;
 
 void *send_msg(void *args)
 {
-    func_args *ac_args=args;
-    int sockfd=ac_args->sockfd;
-    struct sockaddr_in servaddr=ac_args->servaddr;
-    char msg[1024];
-    while (1)
-    {   
-        memset(&msg, 0, sizeof(msg));
-        printf("Sender : ");
+    func_args *ac_args = args;//typecasted here use ac_args moron developer :)
+    int sockfd = ac_args->sockfd;
+    struct sockaddr_in servaddr = ac_args->servaddr;
 
-        sendto( sockfd, (const char *)msg, strlen(msg), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+    WINDOW * input_box = ac_args->ibox;
+    WINDOW * sender_box = ac_args->sbox; 
+    int line_count=1;
+    char msg[MAXSIZE];
+    while (1)
+    {
+        memset(&msg, 0, sizeof(msg));
+        // printf("Sender : ");
+        // scanf("%s",msg);
+
+        mvwgetstr(input_box,1,1,msg);
+        wmove(input_box,1,1);
+        wclrtoeol(input_box);
+        wrefresh(input_box);
+        mvwprintw(sender_box,line_count,1,msg);
+        wmove(input_box,1,1);
+        wrefresh(sender_box);
+        refresh(); //dal diya bas
+        line_count++;
+
+        sendto(sockfd, (const char *)msg, strlen(msg), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
     }
 }
 void *recv_msg(void *args)
 {
-    
-    func_args *ac_args=args;
-    int sockfd=ac_args->sockfd;
-    struct sockaddr_in servaddr=ac_args->servaddr;
-    int n, len;
-    char buffer[1024];
+
+    func_args *ac_args = args;
+    int sockfd = ac_args->sockfd;
+    struct sockaddr_in servaddr = ac_args->servaddr;
+    int n, len, lc=1;
+    WINDOW * reciever_box = ac_args->rbox;
+    char buffer[MAXSIZE];
     while (1)
     {
         memset(&buffer, 0, sizeof(buffer));
         n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *)&servaddr, (socklen_t *)&len);
-        printf("\t Reciever: %s",buffer);
-
+        // printf("\t Reciever: %s", buffer);
+    
+        mvwprintw(reciever_box,lc,1,buffer);
+        wrefresh(reciever_box);
+        lc++;
+    
     }
 }
-int client_func(int sockfd, char ip_addr[], int port)
+int client_func(int sockfd, char ip_addr[], int port,WINDOW* ibox, WINDOW* sbox, WINDOW* rbox)
 {
 
     char buffer[MAXLINE];
@@ -56,7 +82,7 @@ int client_func(int sockfd, char ip_addr[], int port)
     servaddr.sin_port = htons(port);
     servaddr.sin_addr.s_addr = inet_addr(ip_addr);
     int n, len;
-    if(connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
     {
         printf("\n Error : Connect Failed \n");
         exit(0);
@@ -67,6 +93,10 @@ int client_func(int sockfd, char ip_addr[], int port)
     func_args *args = malloc(sizeof *args);
     args->sockfd = sockfd;
     args->servaddr = servaddr;
+
+    args->ibox=ibox;
+    args->sbox=sbox;
+    args->rbox=rbox;
 
     int rc1 = pthread_create(&threads[0], NULL, send_msg, (void *)args);
     if (rc1)
@@ -80,7 +110,7 @@ int client_func(int sockfd, char ip_addr[], int port)
         printf("ERROR; return code from pthread_create() is %d\n", rc2);
         exit(-1);
     }
-    int ret1 = pthread_join(threads[0],NULL);
-    int ret2 = pthread_join(threads[1],NULL);
+    int ret1 = pthread_join(threads[0], NULL);
+    int ret2 = pthread_join(threads[1], NULL);
     return 0;
 }
--- test.c
+++ test.c
@@ -3,14 +3,36 @@
 #include <string.h>
 int main()
 {
+
     char ip_address[50];
     int port_num;
     struct stun_info s1;
     s1=stun_in();
-    printf("%s:%d\nsockfd=%d",s1.ip, s1.port,s1.sockfd);
+    printf("%s:%d\n",s1.ip, s1.port,s1.sockfd);
     printf("Enter ip address : ");
     scanf("%s",ip_address);
     printf("Enter port : ");
     scanf("%d",&port_num);
-    client_func(s1.sockfd,ip_address,port_num);
+        initscr();
+    cbreak();
+
+    int ymax,xmax;
+    getmaxyx(stdscr,ymax,xmax);
+    WINDOW *input_box = newwin(4,xmax-12,ymax-6,6);
+    box(input_box,0,0);
+    refresh();
+    wrefresh(input_box);
+    
+    
+    WINDOW *sender_box = newwin(ymax-6,(xmax-12)/2,0,6);
+    box(sender_box,0,0);
+    refresh();
+    wrefresh(sender_box);
+    
+    WINDOW *reciever_box = newwin(ymax-6,(xmax-12)/2,0,6+(xmax-12)/2);
+    box(reciever_box,0,0);
+    refresh();
+    wrefresh(reciever_box);
+    
+    client_func(s1.sockfd,ip_address,port_num,input_box,sender_box,reciever_box);
 }
